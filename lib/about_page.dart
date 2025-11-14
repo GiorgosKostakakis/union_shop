3e2a1b1acd4ca0b089975369a3ce7be495dd61e2
GiorgosKostakakis <giorgoskistakakis@gmail.com>
2025-11-14 00:47:16 +0000

    update About Us section

--- lib/about_page.dart
+++ lib/about_page.dart
@@ -59,7 +59,12 @@ class AboutPage extends StatelessWidget {
                   ),
                   SizedBox(height: 16),
                   Text(
-                    'Union Shop has been serving the University of Portsmouth student community for years. We are committed to providing quality products and services that enhance your university experience.',
+                    'Welcome to the Union Shop!\n\n'
+                    'We\'re dedicated to giving you the very best University branded products, with a range of clothing and merchandise available to shop all year round! We even offer an exclusive personalisation service!\n\n'
+                    'All online purchases are available for delivery or instore collection!\n\n'
+                    'We hope you enjoy our products as much as we enjoy offering them to you. If you have any questions or comments, please don\'t hesitate to contact us at hello@upsu.net.\n\n'
+                    'Happy shopping!\n\n'
+                    'The Union Shop & Reception Team',
                     style: TextStyle(
                       fontSize: 16,
                       height: 1.5,
