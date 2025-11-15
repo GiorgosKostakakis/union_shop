d98e5565f360b23a334d0dea832c734440d4de79
GiorgosKostakakis <giorgoskistakakis@gmail.com>
2025-11-15 18:03:03 +0000

    Add Footer widget class

--- lib/main.dart
+++ lib/main.dart
@@ -466,3 +466,17 @@ class ProductCard extends StatelessWidget {
     );
   }
 }
+
+class Footer extends StatelessWidget {
+  const Footer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      color: Colors.grey[50],
+      padding: const EdgeInsets.all(24),
+      child: const Text('Footer'),
+    );
+  }
+}
