e92d9d661a97f222cc831edd38d9a7c736f416ae
GiorgosKostakakis <giorgoskistakakis@gmail.com>
2025-11-16 17:47:57 +0000

    Create basic CollectionsPage scaffold

---
+++ lib/collections_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class CollectionsPage extends StatelessWidget {
+  const CollectionsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Collections Page'),
+      ),
+    );
+  }
+}
