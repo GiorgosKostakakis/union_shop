e2773e399fec3362abc4ad3989481dd56f7671b9
GiorgosKostakakis <giorgoskistakakis@gmail.com>
2025-11-19 16:10:56 +0000

    fix the duplicate header issue

--- lib/collection_page.dart
+++ lib/collection_page.dart
@@ -19,27 +19,6 @@ class CollectionPage extends StatelessWidget {
         child: Column(
           children: [
             // Header
-            Container(
-              height: 100,
-              color: Colors.white,
-              child: Column(
-                children: [
-                  // Top banner
-                  Container(
-                    width: double.infinity,
-                    padding: const EdgeInsets.symmetric(vertical: 8),
-                    color: const Color(0xFF4d2963),
-                    child: const Text(
-                      'PLACEHOLDER HEADER TEXT',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(color: Colors.white, fontSize: 16),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-
-            // Header
             const Header(),
 
             // Page Title (minimal skeleton)
