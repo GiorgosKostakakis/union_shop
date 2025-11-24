2ba2ee8e3463e89093585877566ebbf07bcc5174
GiorgosKostakakis <giorgoskistakakis@gmail.com>
2025-11-24 21:21:05 +0000

    Add id field to Collection model

--- lib/models/collection.dart
+++ lib/models/collection.dart
@@ -1,11 +1,13 @@
 import 'package:union_shop/models/product.dart';
 
 class Collection {
+  final String id;
   final String title;
   final String imageUrl;
   final List<Product> products;
 
   const Collection({
+    required this.id,
     required this.title,
     required this.imageUrl,
     required this.products,
