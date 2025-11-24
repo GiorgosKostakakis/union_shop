d00ef7fb55209a8a8fa8ead687584cd27e7c5b7f
GiorgosKostakakis <giorgoskistakakis@gmail.com>
2025-11-24 21:21:35 +0000

    Add 'id' field to Product model

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -1,9 +1,11 @@
 class Product {
+  final String id;
   final String title;
   final String price;
   final String imageUrl;
 
   const Product({
+    required this.id,
     required this.title,
     required this.price,
     required this.imageUrl,
