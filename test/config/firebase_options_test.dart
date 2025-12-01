be4fa4a8ba9dcbba0a0b2d3f682d254588aada1f
GiorgosKostakakis <giorgoskistakakis@gmail.com>
2025-12-01 17:19:05 +0000

    Add tests for DefaultFirebaseOptions to verify platform-specific configurations

--- test/config/firebase_options_test.dart
+++ test/config/firebase_options_test.dart
@@ -1,8 +1,73 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:union_shop/config/firebase_options.dart';
 
 void main() {
   group('DefaultFirebaseOptions Tests', () {
+    test('currentPlatform returns correct options for web', () {
+      debugDefaultTargetPlatformOverride = null;
+      // This test runs on the current platform, so we just verify it doesn't throw
+      expect(() => DefaultFirebaseOptions.currentPlatform, returnsNormally);
+    });
+
+    test('currentPlatform throws for unsupported linux platform', () {
+      debugDefaultTargetPlatformOverride = TargetPlatform.linux;
+      expect(
+        () => DefaultFirebaseOptions.currentPlatform,
+        throwsA(isA<UnsupportedError>().having(
+          (e) => e.message,
+          'message',
+          contains('linux'),
+        )),
+      );
+      debugDefaultTargetPlatformOverride = null;
+    });
+
+    test('currentPlatform returns android options for android platform', () {
+      debugDefaultTargetPlatformOverride = TargetPlatform.android;
+      final options = DefaultFirebaseOptions.currentPlatform;
+      expect(options.projectId, equals('union-3c208'));
+      expect(options.apiKey, equals(DefaultFirebaseOptions.android.apiKey));
+      debugDefaultTargetPlatformOverride = null;
+    });
+
+    test('currentPlatform returns ios options for iOS platform', () {
+      debugDefaultTargetPlatformOverride = TargetPlatform.iOS;
+      final options = DefaultFirebaseOptions.currentPlatform;
+      expect(options.projectId, equals('union-3c208'));
+      expect(options.apiKey, equals(DefaultFirebaseOptions.ios.apiKey));
+      debugDefaultTargetPlatformOverride = null;
+    });
+
+    test('currentPlatform returns macos options for macOS platform', () {
+      debugDefaultTargetPlatformOverride = TargetPlatform.macOS;
+      final options = DefaultFirebaseOptions.currentPlatform;
+      expect(options.projectId, equals('union-3c208'));
+      expect(options.apiKey, equals(DefaultFirebaseOptions.macos.apiKey));
+      debugDefaultTargetPlatformOverride = null;
+    });
+
+    test('currentPlatform returns windows options for windows platform', () {
+      debugDefaultTargetPlatformOverride = TargetPlatform.windows;
+      final options = DefaultFirebaseOptions.currentPlatform;
+      expect(options.projectId, equals('union-3c208'));
+      expect(options.apiKey, equals(DefaultFirebaseOptions.windows.apiKey));
+      debugDefaultTargetPlatformOverride = null;
+    });
+
+    test('currentPlatform returns fuchsia options throws for unsupported platform', () {
+      debugDefaultTargetPlatformOverride = TargetPlatform.fuchsia;
+      expect(
+        () => DefaultFirebaseOptions.currentPlatform,
+        throwsA(isA<UnsupportedError>().having(
+          (e) => e.message,
+          'message',
+          contains('not supported for this platform'),
+        )),
+      );
+      debugDefaultTargetPlatformOverride = null;
+    });
+
     test('web options are defined', () {
       expect(DefaultFirebaseOptions.web.apiKey, isNotEmpty);
       expect(DefaultFirebaseOptions.web.appId, isNotEmpty);
