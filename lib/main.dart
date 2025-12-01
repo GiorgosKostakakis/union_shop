57849023bcd7e8ca066842c3e603f3973fbde54d
GiorgosKostakakis <giorgoskistakakis@gmail.com>
2025-12-01 19:33:40 +0000

    Add navigation to clothing collections from HomeScreen button

--- lib/main.dart
+++ lib/main.dart
@@ -263,7 +263,9 @@ class HomeScreen extends StatelessWidget {
                         ),
                         const SizedBox(height: 32),
                         ElevatedButton(
-                          onPressed: null,
+                          onPressed: () {
+                            context.go('/collections/clothing');
+                          },
                           style: ElevatedButton.styleFrom(
                             backgroundColor: const Color(0xFF4d2963),
                             foregroundColor: Colors.white,
