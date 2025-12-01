da42a6a4d8c4f5311db0c9abd3f846a39fd7b4d2
GiorgosKostakakis <giorgoskistakakis@gmail.com>
2025-12-01 20:55:26 +0000

    Add description field to Product model for enhanced product details

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -3,11 +3,13 @@ class Product {
   final String title;
   final String price;
   final String imageUrl;
+  final String description;
 
   const Product({
     required this.id,
     required this.title,
     required this.price,
     required this.imageUrl,
+    this.description = '',
   });
 }
