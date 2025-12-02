571b0e52b75e4d8d22a7026dab0d4268b7258990
GiorgosKostakakis <giorgoskistakakis@gmail.com>
2025-12-02 00:51:57 +0000

    Remove unused import for fixtures in header_test.dart

--- test/widgets/header_test.dart
+++ test/widgets/header_test.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:union_shop/main.dart';
-import 'package:union_shop/models/fixtures.dart';
 import '../test_helpers.dart';
 
 void main() {
